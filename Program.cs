49923f7bc2e0e44bb2b5f66621de5ff2ca6661b9
Trần Nhật Long <longlun1710@gmail.com>
2026-09-14 18:57:35 +0700

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Channels;
-
-static int NhapSoNguyen(string mess)
-{
-    Console.Write(mess);
-    return int.Parse(Console.ReadLine());
-}
-int n = NhapSoNguyen("Nhap n :");
-Console.WriteLine("Ban vua nhap :" + n);
