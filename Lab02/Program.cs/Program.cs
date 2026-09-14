e66827a2ff586822199fa7295fdaa5e611ab3875
Trần Nhật Long <longlun1710@gmail.com>
2026-09-14 19:01:31 +0700

    Add files via upload

---
+++ Lab02/Program.cs/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Channels;
+
+static int NhapSoNguyen(string mess)
+{
+    Console.Write(mess);
+    return int.Parse(Console.ReadLine());
+}
+int n = NhapSoNguyen("Nhap n :");
+Console.WriteLine("Ban vua nhap :" + n);
