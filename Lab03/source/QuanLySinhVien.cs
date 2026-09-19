cba94f187e5344e35c7c9d9b32077c4c05c2ad80
Trần Nhật Long <longlun1710@gmail.com>
2026-09-19 20:39:13 +0700

    Update QuanLySinhVien.cs

--- Lab03/source/QuanLySinhVien.cs
+++ Lab03/source/QuanLySinhVien.cs
@@ -1,1 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab03
+{
+    public class QuanLySinhVien
+    {
+        private List<SinhVien> ds = new List<SinhVien>();
+
+        public void Them(SinhVien sv)
+        {
+            if (ds.Any(x => x.MaSinhVien == sv.MaSinhVien))
+                throw new Exception("Mã sinh viên đã tồn tại");
+            ds.Add(sv);
+        }
+
+        public bool SuaDiem(string ma, double diemMoi)
+        {
+            var sv = TimTheoMa(ma);
+            if (sv == null) return false;
+            sv.DiemTrungBinh = diemMoi;
+            return true;
+        }
+
+        public bool Xoa(string ma)
+        {
+            var sv = TimTheoMa(ma);
+            if (sv == null) return false;
+            ds.Remove(sv);
+            return true;
+        }
+
+        public SinhVien TimTheoMa(string ma)
+        {
+            return ds.FirstOrDefault(x => x.MaSinhVien == ma);
+        }
+
+        public List<SinhVien> TimTheoTen(string ten)
+        {
+            return ds.Where(x => x.HoTen.Contains(ten)).ToList();
+        }
+
+        public List<SinhVien> SapXepTheoDiem()
+        {
+            return ds.OrderByDescending(x => x.DiemTrungBinh).ToList();
+        }
+
+        public List<SinhVien> LocSinhVienDat()
+        {
+            return ds.Where(x => x.DiemTrungBinh >= 5).ToList();
+        }
+
+        public List<SinhVien> LayDanhSach()
+        {
+            return ds;
+        }
+    }
+}
 
