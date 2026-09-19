799d415a7a60bd6d848f02030bff95939ef00f26
Trần Nhật Long <longlun1710@gmail.com>
2026-09-19 20:38:54 +0700

    Update Nguoi.cs

--- Lab03/source/Nguoi.cs
+++ Lab03/source/Nguoi.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab03
+{
+    public class Nguoi
+    {
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+
+        public Nguoi(string hoTen, DateTime ngaySinh)
+        {
+            HoTen = hoTen;
+            NgaySinh = ngaySinh;
+        }
+
+        public virtual string LayThongTin()
+        {
+            return $"Họ tên: {HoTen}, Ngày sinh: {NgaySinh:dd/MM/yyyy}";
+        }
+    }
+}
+
 
