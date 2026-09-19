ed1cd2ac3fc458e5bf39e31070bbd9e41394ce00
Trần Nhật Long <longlun1710@gmail.com>
2026-09-19 20:53:19 +0700

    Update Program.cs

--- Lab03/source/Program.cs
+++ Lab03/source/Program.cs
@@ -10,6 +10,7 @@ namespace Lab03
 
         static void Main()
         {
+            Console.InputEncoding = System.Text.Encoding.UTF8
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             while (true)
