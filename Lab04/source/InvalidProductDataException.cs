40b30442fc56c720de2c77607d8e20dc103a771f
Trần Nhật Long <longlun1710@gmail.com>
2026-09-21 19:31:58 +0700

    Add files via upload

---
+++ Lab04/source/InvalidProductDataException.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab04.Exceptions;
+
+public class InvalidProductDataException : Exception
+{
+    public InvalidProductDataException(string message)
+        : base(message)
+    {
+    }
+}
