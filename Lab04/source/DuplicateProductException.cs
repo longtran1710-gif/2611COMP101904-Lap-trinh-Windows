e1cb8afc484bd39b23c64bbcfec6c787f668a3f9
Trần Nhật Long <longlun1710@gmail.com>
2026-09-21 19:30:42 +0700

    Add files via upload

---
+++ Lab04/source/DuplicateProductException.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab04.Exceptions;
+
+public class DuplicateProductException : Exception
+{
+    public DuplicateProductException(string maSP)
+        : base($"San pham co ma '{maSP}' da ton tai.")
+    {
+        MaSP = maSP;
+    }
+
+    public string MaSP { get; }
+}
