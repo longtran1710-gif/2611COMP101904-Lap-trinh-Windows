8ad0c03db6537e8d60fbbed8ed800ddf59c3f249
Trần Nhật Long <longlun1710@gmail.com>
2026-09-21 19:32:41 +0700

    Add files via upload

---
+++ Lab04/source/ProductNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab04.Exceptions;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException(string maSP)
+        : base($"Khong tim thay san pham co ma '{maSP}'.")
+    {
+        MaSP = maSP;
+    }
+
+    public string MaSP { get; }
+}
